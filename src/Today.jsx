0231069d8d625e776e761c7b0363fb23b352924d
nereuseng <nereuseng@gmail.com>
2017-09-11 23:59:06 +0800

    fixed Today's button click.

--- src/Today.jsx
+++ src/Today.jsx
@@ -13,7 +13,7 @@ export default class Today extends Component{
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover',
             position: 'fixed',
-            zIndex: '-1',
+            // zIndex: '-1',
             width: '100%',
             height: '100%',
         };
