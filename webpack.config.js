a7afaf72107d2c6c896679897b06a5d4fabdc4e0
nereuseng <nereuseng@gmail.com>
2018-01-10 12:58:53 +0800

    webpack config change.

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ module.exports = {
     /*入口*/
     entry: [
         path.join(__dirname, 'src/index.js'),
-        'react-hot-loader/patch',
     ],  
     /*输出到dist文件夹，输出文件名字为bundle.js*/
     output: {
