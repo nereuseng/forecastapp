15ae439b3643d73492e78a4b3c1b203a790b6284
nereuseng <nereuseng@gmail.com>
2018-01-29 11:32:55 +0800

    weekend updated. still figuring express.js

--- server-side/forecastapp-server/src/middleware/access-controller.js
+++ server-side/forecastapp-server/src/middleware/access-controller.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 
-/*
- * Grants access for cross-origin HTTP requests.
- * See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
- */
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
     if (req.get('Access-Control-Request-Headers'))
         res.set('Access-Control-Allow-Headers', req.get('Access-Control-Request-Headers'));
@@ -12,4 +8,4 @@ module.exports = function(req, res, next) {
         res.set('Access-Control-Allow-Methods', req.get('Access-Control-Request-Method'));
 
     next();
-};
+}
--- server-side/forecastapp-server/src/routers/posts.js
+++ server-side/forecastapp-server/src/routers/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const accessController = require('../middleware/access-controller.js');
 
 const postModel = require('../model/posts.js');
 const voteModel = require('../model/votes.js');
@@ -18,4 +19,30 @@ router.get('./posts', function (req, res, next) {
 });
 
 // Create
-router.post('/')
+router.post('/posts', function(req, res, next) {
+    const {mood, text} = req.body;
+    if (!mood || !text) {
+        const err = new Error('Mood and text are required');
+        err.status = 400;
+        throw err;
+    }
+    postModel.create(mood, text).then(post => {
+        res.json(post);
+    }).catch(next);
+});
+
+// Vote
+router.post('/post/:id/:mood(clear|clouds|drizzle|rain|thunder|snow|windy)Votes', function(req, res, next) {
+    const {id, mood} = req.params;
+    if(!id || !mood) {
+        const err = new Err('Post ID and mood are required');
+        err.status = 400;
+        throw err;
+    }
+
+    voteModel.create(id, mood).then(post => {
+        res.json(post);
+    }).catch(next);
+});
+
+module.exports = router;
